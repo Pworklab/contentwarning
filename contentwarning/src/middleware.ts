32036d1785ad4fdb21b9799b398105d6f5a74b14
Pworklab <52058933+Pworklab@users.noreply.github.com>
2024-07-29 23:42:39 +0900

    update

--- contentwarning/src/middleware.ts
+++ contentwarning/src/middleware.ts
@@ -22,7 +22,7 @@ function getLocale(request: NextRequest): string | undefined {
 }
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-
+  
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) =>
       !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
