4587c5dfeba8dc2e68adf2da81f83c301077feb2
Pworklab <52058933+Pworklab@users.noreply.github.com>
2024-07-30 01:15:00 +0900

    update

--- contentwarning/src/get-dictionary.ts
+++ contentwarning/src/get-dictionary.ts
@@ -1,12 +1,22 @@
 import "server-only";
 import type { Locale } from "./i18-config";
 
-const dictionaries = {
-    en: () => import("./dictionaries/en.json").then(module => module.default), // Adjusted path
-    ja: () => import("./dictionaries/ja.json").then(module => module.default), // Adjusted path
+type DictionaryFunction = () => Promise<{
+  header: {
+    home: string;
+    monster: string;
+  };
+  homepage: {
+    greetings: string;
+  };
+}>;
+
+const dictionaries: Record<Locale, DictionaryFunction> = {
+  en: () => import("./dictionaries/en.json").then(module => module.default),
+  ja: () => import("./dictionaries/ja.json").then(module => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => {
-  const dictionaryFunction = dictionaries[locale as keyof typeof dictionaries];
+  const dictionaryFunction = dictionaries[locale];
   return dictionaryFunction ? dictionaryFunction() : dictionaries.ja();
 };
--- contentwarning/src/i18-config.ts
+++ contentwarning/src/i18-config.ts
@@ -1,6 +1,7 @@
 export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "ja"],
-};
-
-export type Locale = typeof i18n.locales[number];
+    defaultLocale: "en",
+    locales: ["en", "ja"],
+  } as const;
+  
+  export type Locale = (typeof i18n)["locales"][number];
+  
