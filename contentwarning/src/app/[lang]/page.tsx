80f8813173d2e13d8d347b539838d43350077b1d
Pworklab <52058933+Pworklab@users.noreply.github.com>
2024-07-31 01:52:21 +0900

    add corousel

--- contentwarning/src/app/[lang]/page.tsx
+++ contentwarning/src/app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 import { getDictionary } from "p/get-dictionary";
 import { Locale } from "p/i18-config";
-
+import Carousel from "p/components/carousel";
 export default async function Home({
   params: { lang },
 }: {
@@ -8,10 +8,16 @@ export default async function Home({
 })
 {
   const dictionary = await getDictionary(lang);
+  const DATA = [
+  { image: 'https://64.media.tumblr.com/c5da212785afe45cd213c76d4ac05f4b/670bee06beca26c2-48/s2048x3072/8b3bc84435fb85fe9f4bb89ef216fff5df6dd08e.pnj' },
+  { image: 'https://64.media.tumblr.com/ec8bad45e46767a95378f119b3b106ef/670bee06beca26c2-b5/s2048x3072/a5e13aab8fccd0608be3973d36e4e3efa8b7d7d9.pnj' },
+  { image: 'https://64.media.tumblr.com/f666dfb0f72d34f40fe5c20d6267333a/670bee06beca26c2-24/s2048x3072/9d234017b7d8efeb06a2c89f4a8201313d1d13d2.pnj' },
+  { image: 'https://64.media.tumblr.com/fd34f98d44a960bc40c97eb513faa17c/670bee06beca26c2-e8/s2048x3072/7fd11ff8494f60d7e766297cdb834325905c1df2.pnj' },
+]
   return (
     <>
-    <div className="w-auto flex justify-center">
-      <div className="mx-7">
+    <main className="w-auto justify-center flex-row space-y-10">
+      <section className="mx-7">
       <div className="w-full h-[200px] border-2 border-minty bg-lefu ">
       <div className="w-[80px] h-[80px] border-2 m-2 border-minty bg-iconshiro bg-center bg-cover hover:bg-iconkuro">
       </div>
@@ -21,8 +27,13 @@ export default async function Home({
         </p>
       </div>
       </div>
-      </div>
-    </div>    
+      </section>
+     
+      <section className="flex min-h-screen flex-col items-center text-center">
+      <Carousel data={DATA} />
+      </section>
+     
+      </main>
    </>
   );
 }
